82b2e520090251c276178a75b871b027decc7339
nissi1234 <150512493+nissi1234@users.noreply.github.com>
2024-02-21 18:40:55 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "vpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   tags = {
-    Name    = "Demo VPC"
+    Name    = "VPC"
     Project = "Terraform"
   }
 }
